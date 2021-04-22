cfc9396a4b3c74fb9b92007d66ec79bc1273e9df
wuyuanwuhui99 <275018723@qq.com>
2021-04-23 00:00:29 +0800

    修改个人信息页面

--- lib/pages/edit_page.dart
+++ lib/pages/edit_page.dart
@@ -3,6 +3,7 @@ import '../service/server_method.dart';
 import 'package:provider/provider.dart';
 import '../provider/UserInfoProvider.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class EditPage extends StatefulWidget {
   final String title;//编辑的标题
@@ -34,61 +35,70 @@ class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
     Map userInfo = Provider.of<UserInfoProvider>(context).userInfo;
+    return
+      FlutterEasyLoading(
+        child: Scaffold(
+          body:
+          Padding(padding: EdgeInsets.only(left: 20,right: 20,top: 20),child: Column(
+            children: <Widget>[
+              Container(
+                height: 40,
+                child:
+                Stack(
+                    overflow: Overflow.visible,
+                    children: <Widget>[
+                      Center(child: Text(widget.title)),
+                      Positioned(
+                          top: 0,
+                          right: 0,
+                          child:
+                          InkWell(
+                              onTap: ()async{
+                                if(!hasChange)return;
+                                if(!widget.isAllowEmpty && myController.text == ""){
+                                  Fluttertoast.showToast(
+                                      msg: "${widget.title}不能为空",
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.CENTER,
+                                      fontSize: 16.0
+                                  );
+                                  return;
+                                }
+                                await EasyLoading.show();
+                                Map myUserInfo = Map.from(userInfo);
+                                myUserInfo[widget.field] = myController.text;
+                                updateUserData(myUserInfo).then((value)async{
+                                  setState(() {
+                                    hasChange = false;
+                                  });
+                                  Provider.of<UserInfoProvider>(context).setUserInfo(myUserInfo);
+                                  await EasyLoading.dismiss(animation: true);
+                                  Navigator.pop(context);
+                                }).catchError((){
 
-    return Scaffold(
-        body:
-        Padding(padding: EdgeInsets.only(left: 20,right: 20,top: 20),child: Column(
-          children: <Widget>[
-            Container(
-              height: 40,
-              child:
-              Stack(
-                  overflow: Overflow.visible,
-                  children: <Widget>[
-                    Center(child: Text(widget.title)),
-                    Positioned(
-                        top: 0,
-                        right: 0,
-                        child:
-                        InkWell(
-                          onTap: (){
-                            if(!hasChange)return;
-                            if(!widget.isAllowEmpty && myController.text == ""){
-                              Fluttertoast.showToast(
-                                  msg: "${widget.title}不能为空",
-                                  toastLength: Toast.LENGTH_SHORT,
-                                  gravity: ToastGravity.CENTER,
-                                  fontSize: 16.0
-                              );
-                              return;
-                            }
-                            Map myUserInfo = Map.from(userInfo);
-                            myUserInfo[widget.field] = myController.text;
-                            updateUserData(myUserInfo).then((value){
-                              Provider.of<UserInfoProvider>(context).setUserInfo(myUserInfo);
-                            });
-                          },
-                          child: Container(
-                            width: 70,
-                            height: 35,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                              color: hasChange ? Color.fromRGBO(24, 144, 255,1) : Color.fromRGBO(221, 221, 221,1),
-                            ),
-                            child: Center(child: Text('保存',style: TextStyle(color: hasChange ? Colors.white : Color.fromRGBO(204, 204, 204,1)),))
+                                });
+                              },
+                              child: Container(
+                                  width: 70,
+                                  height: 35,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                                    color: hasChange ? Color.fromRGBO(24, 144, 255,1) : Color.fromRGBO(221, 221, 221,1),
+                                  ),
+                                  child: Center(child: Text('保存',style: TextStyle(color: hasChange ? Colors.white : Color.fromRGBO(204, 204, 204,1)),))
+                              )
                           )
-                        )
-                    )]),
-            ),
-            TextField(
-              controller: myController,
-              onChanged: (value){
-                setState(() {
-                  hasChange = true;
-                });
-              },)
-          ],
-        ),)
-    );
+                      )]),
+              ),
+              TextField(
+                controller: myController,
+                onChanged: (value){
+                  setState(() {
+                    hasChange = true;
+                  });
+                },)
+            ],
+          ),)
+      ),);
   }
 }
