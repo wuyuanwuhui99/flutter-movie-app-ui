237e70542909739d44952dc34dbf13460aa27dad
wuyuanwuhui99 <275018723@qq.com>
2024-08-29 23:13:43 +0800

    修改shell脚本

--- 一键提交到gitee.bat
+++ 一键提交到gitee.bat
@@ -1,3 +1,3 @@
 git remote rm origin
 git remote add origin https://gitee.com/wuyuanwuhui99/Harmony-arkts-movie-music-app-ui.git
-git push origin main
+git push origin master
---
+++ 一键提交到github.bat
@@ -0,0 +1,3 @@
+git remote rm origin
+git remote add origin https://github.com/wuyuanwuhui99/Harmony-arkts-movie-music-app-ui.git
+git push origin master
