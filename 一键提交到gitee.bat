6b6871dc0e55cf846df8e4f27ecdc2bd4fb90148
wuyuanwuhui99 <275018723@qq.com>
2024-08-29 23:10:31 +0800

    修改shell脚本

--- 一键提交到gitee.bat
+++ 一键提交到gitee.bat
@@ -1,3 +1,3 @@
-git remote rm github
-git remote add gitee https://gitee.com/wuyuanwuhui99/Harmony-arkts-movie-music-app-ui.git
+git remote rm origin
+git remote add origin https://gitee.com/wuyuanwuhui99/Harmony-arkts-movie-music-app-ui.git
 git push origin main
