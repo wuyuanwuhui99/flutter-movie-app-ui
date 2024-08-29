189745e871a7e81917d4973ce5f5df8c54c0b46c
wuyuanwuhui99 <275018723@qq.com>
2024-08-29 23:45:15 +0800

    修改shell脚本

--- 一键提交到gitee和github.bat
+++ 一键提交到gitee和github.bat
@@ -1,3 +1,6 @@
 git remote rm origin
 git remote add origin https://gitee.com/wuyuanwuhui99/flutter-movie-app-ui.git
 git push origin main
+git remote rm origin
+git remote add origin https://github.com/wuyuanwuhui99/flutter-movie-app-ui
+git push origin main
--- 一键提交到github.bat
+++
@@ -1,3 +0,0 @@
-git remote rm origin
-git remote add origin https://github.com/wuyuanwuhui99/flutter-movie-app-ui
-git push origin main
